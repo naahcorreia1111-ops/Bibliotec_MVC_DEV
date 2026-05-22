82d85b01cf865e9859ff42f90e4882afacfc2399
THAYNA CORREIA DOS SANTOS <49867204816@info.local>
2026-05-22 16:54:10 -0300

    Criação do projeto e criação da model de Usuario

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bibliotec_MVC_DEV.Controllers;
+
+public class HomeController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+}
